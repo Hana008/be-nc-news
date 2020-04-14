a1cee3e77095eac36fdf7e5beb6911ed6d46195b
Hana Mohamed <hanam@Hanas-MacBook-Pro.local>
2020-04-14 14:26:23 +0100

    add cors

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const apiRouter = require('./routes/apiRouter');
 const { errorHandler } = require('./errors/index');
-const { send404Error } = require('./errors/index')
+const { send404Error } = require('./errors/index');
+const cors = require('cors');
+
+app.use(cors());
 
 app.use(express.json());
 
